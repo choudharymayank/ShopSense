ea158d3a1ceb7f1e70a4bd260fdef5704185e51a
choudharymayank <choudharymayank6919@gmail.com>
2024-06-27 14:59:49 +0530

    Deploy

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
-            src="/images/contactus.jpeg"
+            src="/Images/contactus.JPEG"
             alt="contactus"
             style={{ width: "100%" }}
           />
